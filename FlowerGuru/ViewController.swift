17205770ef2692403f1f09bc7873790a028ab66e
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2019-06-17 15:36:44 -0300

    Create interface for the user to choose a photo from the library or take one with the camera.

--- FlowerGuru/ViewController.swift
+++ FlowerGuru/ViewController.swift
@@ -8,13 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    private let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var searchButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
     }
-
-
+    
+    @IBAction func searchTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, withSourceType: .photoLibrary)
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, withSourceType: .camera)
+    }
+    
+    private func present(_ imagePicker: UIImagePickerController, withSourceType sourceType: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            imagePicker.sourceType = sourceType
+            present(imagePicker, animated: true, completion: nil)
+            
+        } else {
+            var sourceTypeString = ""
+            
+            switch(sourceType) {
+            case .camera: sourceTypeString = "Camera"
+            case .photoLibrary: sourceTypeString = "Photo Library"
+            case .savedPhotosAlbum: sourceTypeString = "Photos Album"
+            default: sourceTypeString = "Unknown Source Type"
+            }
+            
+            print("Sorry, \(sourceTypeString) source type is not available.")
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
